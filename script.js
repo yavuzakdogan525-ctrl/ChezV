0d4e202256ed3c3ca26f50ae47139a7acd256694
yavuzakdogan525-ctrl <yavuzakdogan525@gmail.com>
2026-01-22 14:50:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const slides = document.querySelectorAll(".bg-slide");
   let index = 0;
 
-  if (!slides.length) return;
+  if (slides.length === 0) return;
 
   setInterval(() => {
-    slides.forEach(s => s.classList.remove("active"));
+    slides[index].classList.remove("active");
     index = (index + 1) % slides.length;
     slides[index].classList.add("active");
   }, 3500);
